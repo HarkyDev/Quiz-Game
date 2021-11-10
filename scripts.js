aa8f9d469d74fabc15b55f437e102a2e05f58d46
Eoin Harkin <eharky@gmail.com>
2021-11-10 13:56:43 +0000

    added 5min timer function that diplays on game start

--- scripts.js
+++ scripts.js
@@ -6,33 +6,16 @@
 const startButton = document.getElementById('start-btn')
 const mainGame = document.getElementById('game-container')
 
+
 startButton.addEventListener('click', startQuiz);
 
 function startQuiz(){
     console.log("game is started")
     startButton.classList.add('hide');
-    mainGame.classList.remove('hide');     
+    mainGame.classList.remove('hide'); 
+    startTimer()    
 }
 
-
-//timer  function
-let timerDisplay = 5
-
-console.log(timerDisplay)
-var timerMinusSeconds = function(){
-    timerDisplay --
-    console.log(timerDisplay)
-    if (timerDisplay==0){
-    clearInterval(timerId)
-    }
-}
-
-const timerId = setInterval(timerMinusSeconds,1000)
-
-timerExpiration = function(){
-    if (timerDisplay == 0)
-        console.log("timer should stop")
-}
 
 
 
@@ -50,6 +33,29 @@ function selectAnswer(){
 //question code
 
 //timer code 
+var timerDisplayEl = document.getElementById("timer")
 
-//timer function 
+//timer  function
+//300s = 5minutes
+
+var startTimer = function(){
+    let timerValue = 300
+    console.log(timerValue)
+    var timerMinusSeconds = function(){
+        timerValue --
+        console.log(timerValue)
+        timerDisplayEl.innerHTML = "seconds left: " + timerValue
+        if (timerValue==0){
+        clearInterval(timerId)
+        }
+    }
+
+    const timerId = setInterval(timerMinusSeconds,1000)
+
+    timerExpiration = function(){
+        if (timerDisplay == 0)
+            console.log("timer should stop")
+    }
+}
+
 
